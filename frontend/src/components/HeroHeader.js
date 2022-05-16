7f16c1b988037cbcfaee5c56cb94a24c2ec194a0
nisharmultani <nisharmultani7@gmail.com>
2022-05-17 02:14:10 +0530

    feat:Hero_header_and_Navbar

---
+++ frontend/src/components/HeroHeader.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const HeroHeader = () => {
+  return (
+    <header className="bg-white dark:bg-gray-800">
+        <div className="container px-6 py-16 mx-auto">
+            <div className="items-center lg:flex">
+                <div className="w-full lg:w-1/2">
+                    <div className="lg:max-w-lg">
+                        <h1 className="text-2xl font-semibold text-gray-800 uppercase dark:text-white lg:text-3xl">Best Place To Choose Your Clothes</h1>
+                        <p className="mt-2 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
+                        <button className="w-full px-3 py-2 mt-6 text-xs font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Shop Now</button>
+                    </div>
+                </div>
+        
+                <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+                    <img className="w-full h-full lg:max-w-2xl" src={"img/heroHeader.svg"} alt="Catalogue-pana.svg" />
+                </div>
+            </div>
+        </div>
+    </header>
+  )  }
